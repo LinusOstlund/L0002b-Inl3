8ff5af636084d1f16b96665a76bf34adab3f718b
LinusOstlund <54594683+LinusOstlund@users.noreply.github.com>
2021-08-19 14:48:45 +0200

    Add files via upload

---
+++ Inl3-Form/Inl3/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace Inl3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submit = new System.Windows.Forms.Button();
+            this.firstNameBox = new System.Windows.Forms.TextBox();
+            this.lastNameBox = new System.Windows.Forms.TextBox();
+            this.ssnBox = new System.Windows.Forms.TextBox();
+            this.titel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultBox = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // submit
+            // 
+            this.submit.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.submit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit.Location = new System.Drawing.Point(87, 118);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(162, 23);
+            this.submit.TabIndex = 0;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = false;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // firstNameBox
+            // 
+            this.firstNameBox.Location = new System.Drawing.Point(87, 40);
+            this.firstNameBox.Name = "firstNameBox";
+            this.firstNameBox.Size = new System.Drawing.Size(162, 20);
+            this.firstNameBox.TabIndex = 1;
+            // 
+            // lastNameBox
+            // 
+            this.lastNameBox.Location = new System.Drawing.Point(87, 66);
+            this.lastNameBox.Name = "lastNameBox";
+            this.lastNameBox.Size = new System.Drawing.Size(162, 20);
+            this.lastNameBox.TabIndex = 2;
+            // 
+            // ssnBox
+            // 
+            this.ssnBox.Location = new System.Drawing.Point(87, 92);
+            this.ssnBox.Name = "ssnBox";
+            this.ssnBox.Size = new System.Drawing.Size(162, 20);
+            this.ssnBox.TabIndex = 3;
+            // 
+            // titel
+            // 
+            this.titel.AutoSize = true;
+            this.titel.Font = new System.Drawing.Font("Mistral", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titel.Location = new System.Drawing.Point(116, 11);
+            this.titel.Name = "titel";
+            this.titel.Size = new System.Drawing.Size(110, 26);
+            this.titel.TabIndex = 4;
+            this.titel.Text = "VÄLKOMMEN";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Förnamn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Efternamn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Personnummer";
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(87, 167);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(162, 96);
+            this.resultBox.TabIndex = 8;
+            this.resultBox.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(84, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Resultat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 280);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.titel);
+            this.Controls.Add(this.ssnBox);
+            this.Controls.Add(this.lastNameBox);
+            this.Controls.Add(this.firstNameBox);
+            this.Controls.Add(this.submit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.TextBox firstNameBox;
+        private System.Windows.Forms.TextBox lastNameBox;
+        private System.Windows.Forms.TextBox ssnBox;
+        private System.Windows.Forms.Label titel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox resultBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Inl3-Form/Inl3/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Inl3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            resultBox.Clear();
+            String ssn = ssnBox.Text;
+            if (!checkValidSSN(ssn) || ssn == "")
+            {
+                MessageBox.Show("Fel format på personnummer", "Fel format!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+               
+                Person p = new Person(firstNameBox.Text, lastNameBox.Text, ssn);
+                //Print the persons information.
+                resultBox.AppendText(p.ToString());
+            }
+            
+        }
+
+        /**
+         * Kollar att det inmatade personnumret är i rätt format, ååmmdd-xxxx
+         * 
+         */
+        private Boolean checkSSNFormat(String input)
+        {
+            string pattern;
+            // yy (1900-2009)
+            pattern = @"[0-9][0-9]";
+            // mm (01-12)
+            pattern += @"(0[1-9]|1[0-2])";
+            // dd (01-31)
+            pattern += @"(0[1-9]|1[0-9]|2[0-9]|3[0-1])";
+            // xxxx (0000 - 9999)
+            pattern += @"[0-9]{4}$";
+            Regex rgx = new Regex(pattern);
+            return rgx.IsMatch(input);
+        }
+
+        /**
+         * Kollar om personnumret är korrekt enligt 21Algoritmen.
+         * Tar in personnumret som ååmmdd-xxxx
+         */
+        private Boolean checkValidSSN(String input)
+        {
+            input = input.Replace("-", "");
+            
+            if(!checkSSNFormat(input)) return false;
+
+            int sum = 0;
+            
+            for(int i=0,multiplier = 0;i<input.Length;i++)
+            {
+                Char c = input[i];
+                multiplier = i % 2 == 0 ? 2 : 1;
+                int res = Convert.ToInt32(c.ToString()) * multiplier;
+
+                sum += getNumberSum(res);
+            }
+            return sum % 10 == 0;
+            
+        }
+
+        /**
+         * Returnerar talsumman enligt 21algoritmen. 
+         * Exempel : 14 ger 1+4 = 5, 21 ger 2 + 1 = 3
+         */
+        private int getNumberSum(int number)
+        {
+            if (number < 10) return number;
+            int sum = 0,m;
+            while(number > 0)
+            {
+                m = number % 10;
+                sum += m;
+                number = number / 10;
+            }
+            return sum;
+        }
+
+
+    }
+
+    class Person
+    {
+        private string FName { get; set; }
+        private string LName { get; set; }
+        private string SSN { get; set; }
+        private string Sex { get; set; }
+        public Person(string fName, string lName, string ssn)
+        {
+            this.FName = fName;
+            this.LName = lName;
+            this.SSN = ssn;
+            //If second to last digit is even, return female, else male.
+            this.Sex = Convert.ToInt32(ssn[ssn.Length - 2].ToString()) % 2 == 0 ? "Kvinna" : "Man";
+        }
+
+
+        /**
+         * Skriver över den orginella toString metoden.
+         */
+        public override string ToString()
+        {
+            return "Förnamn: " + FName + "\n"
+                + "Efternamn: " + LName + "\n"
+                + "Personnummer: " + SSN + "\n"
+                + "Kön: " + Sex;
+        }
+
+
+    }
+}
